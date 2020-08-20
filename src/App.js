324e6ca405f1c800bb53c035cd05b72292ec8f3c
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-08-20 13:02:40 -0400

    learning to deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  
   return (
     <main>
       <h1>Create React App + Go API</h1>
       <h2>
-        Deployed with{' '}
+        Learning How to Deploy with{' '}
         <a
           href="https://vercel.com/docs"
           target="_blank"
@@ -48,8 +40,7 @@ function App() {
         .
       </p>
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+     
     </main>
   );
 }
